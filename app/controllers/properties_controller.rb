b8fd093f893621e9e05bec8e73b3e4cca127114c
dayhily <dayhily@gmail.com>
2017-10-23 10:48:40 +0300

    Add actions to property controller

--- app/controllers/properties_controller.rb
+++ app/controllers/properties_controller.rb
@@ -1,2 +1,49 @@
 class PropertiesController < ApplicationController
+	def index
+    @properties = Property.all
+  end
+	
+	def new
+		@property = Property.new
+	end
+		
+	def create
+		@property = Property.new(property_params)
+		if @property.valid?
+			@property.save			
+			redirect_to @property
+    else
+			render action: 'new'
+    end
+	end
+	
+	def edit
+		@property=Property.find(params[:id])
+	end
+	
+	def update
+		@property=Property.find(params[:id])
+		if @property.update(property_params)
+			redirect_to @property
+		else
+			render action: 'edit'
+		end
+	end
+	
+	def show
+		@property=Property.find(params[:id])
+	end
+	
+	def destroy
+		@property=Property.find(params[:id])
+		@property.destroy
+		redirect_to properties_path
+	end
+	
+private
+    
+  def property_params
+    params.require(:property).permit(:property_address, :property_type, :number_of_rooms,
+		:area_size, :property_price, :addr)
+  end
 end
