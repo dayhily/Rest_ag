6fbcc328976fa8b511dd2728e3f28f6b724d863c
dayhily <dayhily@gmail.com>
2017-10-23 10:35:15 +0300

    Add property model validates

--- app/models/property.rb
+++ app/models/property.rb
@@ -1,2 +1,7 @@
 class Property < ApplicationRecord
+	validates :property_type, presence: true
+	validates :property_address, presence: true
+	validates :area_size, numericality: {greater_than_or_equal_to: 0.01}
+	validates :number_of_rooms, numericality: {greater_than_or_equal_to:1}
+	validates :property_price, numericality: {greater_than_or_equal_to: 0.01}
 end
