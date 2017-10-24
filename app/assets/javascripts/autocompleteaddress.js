469052ce98fc3c0c2bfb38fa4b7fcef24bd3cf12
dayhily <dayhily@gmail.com>
2017-10-24 08:02:32 +0300

    Choose country

--- app/assets/javascripts/autocompleteaddress.js
+++ app/assets/javascripts/autocompleteaddress.js
@@ -20,7 +20,7 @@ function initAutocomplete() {
   // location types.
   autocomplete = new google.maps.places.Autocomplete(
       /** @type {!HTMLInputElement} */(document.getElementById('autocomplete')),
-      {types: ['geocode']});
+      {types: ['geocode'], componentRestrictions: {country: 'ru'}});
 
   // When the user selects an address from the dropdown, populate the address
   // fields in the form.
