3c65295079e72b5a97723d716ae158c9c61af9de
dayhily <dayhily@gmail.com>
2017-10-25 11:19:54 +0300

    Add show_error helper

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,9 @@
 module ApplicationHelper
+	def show_errors(object, field_name)
+		if object.errors.full_messages.any?
+			if !object.errors.full_messages_for(field_name).blank?
+				object.errors.full_messages_for(field_name).first
+			end
+		end
+	end 
 end
