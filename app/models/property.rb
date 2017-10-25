1f74597e842d89e5f19bc2f38dabe1ce94eda805
dayhily <dayhily@gmail.com>
2017-10-25 11:19:30 +0300

    Refactoring validation

--- app/models/property.rb
+++ app/models/property.rb
@@ -1,8 +1,5 @@
 class Property < ApplicationRecord
-	validates :country, presence: true
-	validates :locality, presence: true
-	validates :route, presence: true
-	validates :street_number, presence: true
+	validates_presence_of :country, :locality, :route, :street_number
 	validates :property_type, presence: true
 	validates :area_size, numericality: {greater_than_or_equal_to: 0.01}
 	validates :number_of_rooms, numericality: {greater_than_or_equal_to:1}
