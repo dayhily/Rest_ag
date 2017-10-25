740330bff9bf525e2843393e2fba4fd30d3d4b91
dayhily <dayhily@gmail.com>
2017-10-25 11:37:22 +0300

    Refactoring validation and change error message

--- app/models/property.rb
+++ app/models/property.rb
@@ -2,6 +2,6 @@ class Property < ApplicationRecord
 	validates_presence_of :country, :locality, :route, :street_number
 	validates :property_type, presence: true
 	validates :area_size, numericality: {greater_than_or_equal_to: 0.01}
-	validates :number_of_rooms, numericality: {greater_than_or_equal_to:1}
+	validates :number_of_rooms, presence: true
 	validates :property_price, numericality: {greater_than_or_equal_to: 0.01}
 end
