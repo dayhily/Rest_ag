ec1a074d0fcb3eb600697bbf5cf3e21c6828639d
dayhily <dayhily@gmail.com>
2017-11-04 09:17:48 +0300

    Add validation for number of images

--- app/controllers/images_controller.rb
+++ app/controllers/images_controller.rb
@@ -3,7 +3,8 @@ before_action :set_property
 	def create
 		add_more_images(images_params[:images].to_a)
 		@property.save
-		redirect_to @property
+		flash[:error] = @property.errors.full_messages.join("\n")
+		redirect_to property_path(@property)
 	end
 	
 	def destroy
--- app/models/property.rb
+++ app/models/property.rb
@@ -4,6 +4,13 @@ class Property < ApplicationRecord
 	validates :area_size, numericality: {greater_than_or_equal_to: 0.01}
 	validates :number_of_rooms, presence: true
 	validates :property_price, numericality: {greater_than_or_equal_to: 0.01}
-	validates :images, presence: true
+	validate :number_of_image, if: "images?"
 	mount_uploaders :images, ImagesUploader
 end
+
+private
+  def number_of_image
+    if images.size.to_i > 4
+    errors.add(:images, "You cannot upload more than 4 images")
+    end
+  end
