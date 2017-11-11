24cb2ece032ea03e7c702bc8086e75c02c656217
dayhily <dayhily@gmail.com>
2017-11-11 12:35:31 +0300

    Change migration

--- db/migrate/20171023072149_create_properties.rb
+++ db/migrate/20171023072149_create_properties.rb
@@ -12,6 +12,8 @@ class CreateProperties < ActiveRecord::Migration[5.1]
       t.integer :number_of_rooms
       t.integer :area_size
       t.integer :property_price
+      t.string "images", default: [], array: true
+      t.index ["images"], name: "index_properties_on_images", unique: true
 
       t.timestamps
     end
