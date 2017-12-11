6786d2d31d44af688c5039b2bb890f44bd9efe51
dayhily <dayhily@gmail.com>
2017-12-11 09:33:08 +0300

    Activate validates

--- app/models/address.rb
+++ app/models/address.rb
@@ -1,3 +1,4 @@
 class Address < ApplicationRecord
 	belongs_to :property, inverse_of: :address
+	validates_presence_of :country, :locality, :route, :street_number
 end
--- app/models/property.rb
+++ app/models/property.rb
@@ -1,13 +1,12 @@
 class Property < ApplicationRecord
 	has_one :address, inverse_of: :property, dependent: :destroy
 	accepts_nested_attributes_for :address, :allow_destroy => true
-	#validates_presence_of :country, :locality, :route, :street_number
-	#validates :property_type, presence: true
-	#validates :area_size, numericality: {greater_than_or_equal_to: 0.01}
-	#validates :number_of_rooms, presence: true
-	#validates :property_price, numericality: {greater_than_or_equal_to: 0.01}
-	#validates :images, presence: true
-	#validate :number_of_image
+	validates :property_type, presence: true
+	validates :area_size, numericality: {greater_than_or_equal_to: 0.01}
+	validates :number_of_rooms, presence: true
+	validates :property_price, numericality: {greater_than_or_equal_to: 0.01}
+	validates :images, presence: true
+	validate :number_of_image
 	mount_uploaders :images, ImagesUploader
 	paginates_per 5
 end
