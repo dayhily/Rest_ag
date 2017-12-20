3c7cbdabe5dd373e20ba213e87f33aea5b3b1cf3
dayhily <dayhily@gmail.com>
2017-12-20 11:11:12 +0300

    Add email format validation

--- app/models/contact.rb
+++ app/models/contact.rb
@@ -1,4 +1,5 @@
 class Contact < ApplicationRecord
 	validates :email, presence: true
+	validates :email, format: { with: /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\z/i, on: :create }
 	validates :message, presence: true
 end
