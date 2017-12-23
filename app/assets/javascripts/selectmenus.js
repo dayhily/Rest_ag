c407da2f3962f59dd798b29a69aa8cc09b9abcc2
dayhily <dayhily@gmail.com>
2017-12-23 13:35:52 +0300

    Modify search in view

--- app/assets/javascripts/selectmenus.js
+++ app/assets/javascripts/selectmenus.js
@@ -1,37 +1,43 @@
 // "(document).on('turbolinks:load', function() {})"
 // hook in to an event fired by Turbolinks for every page visit
 $(document).on('turbolinks:load', function selectcountry() {
+  $('#administrative_area').prop('disabled', true);
   var areas = $('#administrative_area').html();
   console.log(areas);
   return $('#country').change(function() {
   var country_select, options1;
-   country_select = $(this).find("option:selected").text();
+   country_select = $('#country :selected').text();
    escaped_country = country_select.replace(/([ #;&,.+*~\':"!^$[\]()=>|\/@])/g, '\\$1');
    console.log(country_select);
     options1 = $(areas).filter("optgroup[label=" + escaped_country + "]").html();
     console.log(options1);
     if (options1) {
-      return $('#administrative_area').html('<option value="">Select area').append(options1);
+      $('#administrative_area').html('<option value="">Select area').append(options1);
+      return $('#administrative_area').prop('disabled', false);
     } else {
-      return $('#administrative_area').empty();
+      $('#administrative_area').empty();
+      return $('#administrative_area').prop('disabled', true);
     }
-  })
+  });
 });
 
 $(document).on('turbolinks:load', function selectarea() {
+  $('#locality').prop('disabled', true);
   var localities = $('#locality').html();
   console.log(localities);
     return $('#administrative_area').change(function() {
     var area_select, options2;
-     area_select = $(this).find("option:selected").text();
+     area_select = $('#administrative_area :selected').text();
      escaped_area = area_select.replace(/([ #;&,.+*~\':"!^$[\]()=>|\/@])/g, '\\$1');
      console.log(area_select);
       options2 = $(localities).filter("optgroup[label=" + escaped_area + "]").html();
       console.log(options2);
       if (options2) {
-        return $('#locality').html('<option value="">Select locality').append(options2);
+        $('#locality').html('<option value="">Select locality').append(options2);
+        return $('#locality').prop('disabled', false);
       } else {
-        return $('#locality').empty();
+        $('#locality').empty();
+        return $('#locality').prop('disabled', true);
       }
-    })
+    });
 });
