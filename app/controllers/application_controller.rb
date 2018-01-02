be7cb9705e9bf7e004392142f389ce8560c446f6
dayhily <dayhily@gmail.com>
2018-01-02 10:47:18 +0300

    Configure permitted parameters for Devise

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,15 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+  before_action :configure_permitted_parameters, if: :devise_controller?
+  
+  private
+  
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up) do |user_params|
+      user_params.permit(:username, :email, :password, :password_confirmation, :remember_me)
+    end
+    devise_parameter_sanitizer.permit(:account_update) do |user_params|
+      user_params.permit(:username, :email, :password, :password_confirmation, :remember_me)
+    end
+  end     
 end
